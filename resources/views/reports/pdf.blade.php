17e5e4bcd9bf48b1a8422c5500fa1acafb3a1312
Sumon Mahmud <sclsumonislam@gmail.com>
2025-05-03 21:52:35 +0600

    add bangla font on invoice

--- resources/views/reports/pdf.blade.php
+++ resources/views/reports/pdf.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="bn">
+
 <head>
     <meta charset="UTF-8">
     <style>
@@ -14,10 +15,114 @@
         }
     </style>
 </head>
+
 <body>
-    <h1>বাংলা রিপোর্ট</h1>
-    <p>নাম: সুমন মাহমুদ</p>
-    <p>ইমেইল: sclsumonislam@gmail.com</p>
-    <p>ধন্যবাদ!</p>
+    <!-- Invoice area Starts -->
+    <div class="invoice-area">
+        <div class="invoice-wrapper">
+            <div class="invoice-header">
+                <h1 class="invoice-title" style="text-align:center;">Invoice Heder - Roman Electronices & Furnitures</h1>
+            </div>
+            <div class="invoice-details">
+                <div class="invoice-details-flex">
+                    <div class="invoice-single-details">
+                        <h2 class="invoice-details-title">Bill To:</h2>
+                        <ul class="details-list">
+                            <li class="list">{{ $customer->customer_name }}</li>
+                            <li class="list"> <a href="#">nazmul@gmail.com </a> </li>
+                            <li class="list"> <a href="#"> 0167846483</a> </li>
+                        </ul>
+                    </div>
+                    <div class="invoice-single-details">
+                        <h4 class="invoice-details-title">Ship To:</h4>
+                        <ul class="details-list">
+                            <li class="list"> <strong>City: </strong> Dhaka</li>
+                            <li class="list"> <strong>Area: </strong>Dhanmondi</li>
+                            <li class="list"> <strong>Address: </strong>West Panthapath, Dhanmondi</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="item-description">
+                <h5 class="table-title">Include Services</h5>
+                <table class="custom--table">
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Unit Price</th>
+                            <th>Quantity</th>
+                            <th>Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>House Cleaning</td>
+                            <td>$10</td>
+                            <td>3</td>
+                            <td>$30</td>
+                        </tr>
+                        <tr>
+                            <td>Car Cleaning</td>
+                            <td>$20</td>
+                            <td>2</td>
+                            <td>$40</td>
+                        </tr>
+                        <tr class="table_footer_row">
+                            <td colspan="3"><strong>Package Fee</strong></td>
+                            <td><strong>$70</strong></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="item-description">
+                <div class="table-responsive">
+                    <h5 class="table-title">Orders Details</h5>
+                    <table class="custom--table">
+                        <thead class="head-bg">
+                            <tr>
+                                <th>Buyer Details</th>
+                                <th>Date & Schedule</th>
+                                <th>Amount Details</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <span class="data-span">Name:</span>Nazmul Hoque<br>
+                                    <span class="data-span">Email:</span>Nazmul@gmail.com <br>
+                                    <span class="data-span">Phone: </span>01678985958 <br>
+                                    <span class="data-span">Address:</span>West Panthapath, Dhanmondi
+                                </td>
+                                <td>
+                                    30-9-2022 <br>
+                                    Fri, 10pm
+                                </td>
+                                <td>
+                                    <span class="data-span"> Package Fee:</span>$70 <br>
+                                    <span class="data-span"> Sub Total:</span>$70 <br>
+                                    <span class="data-span"> Tax: </span>$10 <br>
+                                    <span class="data-span"> Total:</span>$80 <br>
+                                    <span class="data-span">Payment Status: </span>Pending
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+            <footer>
+                <h3 style="text-align: center">
+                    Copyright @2023
+                </h3>
+            </footer>
+
+        </div>
+    </div>
+
 </body>
+
 </html>
+
+
