53c8991be6fe4a7bb146e00323a9e7d69d8b9b61
Sumon Mahmud <sclsumonislam@gmail.com>
2025-05-03 21:34:23 +0600

    add bangla font on invoice

--- config/dompdf.php
+++ config/dompdf.php
@@ -3,6 +3,14 @@
 return [
     'font_dir' => storage_path('fonts/'),
     'font_cache' => storage_path('fonts/'),
+    'fonts' => [
+        'solaiman_lipi' => [
+            'R'  => public_path('fonts/SolaimanLipi.ttf'),  // Path to the SolaimanLipi font
+            'B'  => public_path('fonts/SolaimanLipi.ttf'),  // Use the same file for bold if not available
+            'I'  => public_path('fonts/SolaimanLipi.ttf'),  // Italic font (if available)
+            'BI' => public_path('fonts/SolaimanLipi.ttf'),  // Bold Italic font (if available)
+        ],
+    ],
 
     'font_data' => [
         'notosansbengali' => [
@@ -11,7 +19,6 @@ return [
             'useKashida' => 75,
         ],
     ],
-    
     'default_font' => 'notosansbengali',
 
 
