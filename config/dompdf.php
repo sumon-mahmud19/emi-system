e7c4645c439da9e128ebd6c28ca246e2f8506cf6
Sumon Mahmud <sclsumonislam@gmail.com>
2025-05-03 21:48:54 +0600

    add bangla font on invoice

--- config/dompdf.php
+++ config/dompdf.php
@@ -2,17 +2,17 @@
 
 return [
 
-
     'font_dir' => storage_path('fonts/'),
     'font_cache' => storage_path('fonts/'),
 
     'font_data' => [
         'notosansbengali' => [
             'R' => 'NotoSansBengali-Regular.ttf',
-            'useOTL' => 0xFF,
+            'useOTL' => 0xFF, // Required for complex scripts like Bangla
             'useKashida' => 75,
         ],
     ],
+
     'default_font' => 'notosansbengali',
 
 
--- resources/views/reports/pdf.blade.php
+++ resources/views/reports/pdf.blade.php
@@ -3,6 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <style>
+        @font-face {
+            font-family: 'notosansbengali';
+            src: url('{{ storage_path('fonts/NotoSansBengali-Regular.ttf') }}') format('truetype');
+        }
+
         body {
             font-family: 'notosansbengali', sans-serif;
             font-size: 16px;
@@ -10,10 +15,9 @@
     </style>
 </head>
 <body>
-    <h1>চালান রশিদ</h1>
-    <p>নাম: মোঃ হাসান</p>
-    <p>ফোন: ০১৭৬৩৪১৭০৭১</p>
-    <p>পরিমাণ: ৫০০ টাকা</p>
+    <h1>বাংলা রিপোর্ট</h1>
+    <p>নাম: সুমন মাহমুদ</p>
+    <p>ইমেইল: sclsumonislam@gmail.com</p>
     <p>ধন্যবাদ!</p>
 </body>
 </html>
