2c4eca3beff36afc1df99cafaef37aa3f9e1e964
Sumon Mahmud <sclsumonislam@gmail.com>
2025-05-03 21:45:09 +0600

    add bangla font on invoice

--- resources/views/reports/pdf.blade.php
+++ resources/views/reports/pdf.blade.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html lang="bn">
-
 <head>
     <meta charset="UTF-8">
     <style>
         body {
-            font-family: 'notosansbengali';
-            font-size: 14px;
+            font-family: 'notosansbengali', sans-serif;
+            font-size: 16px;
         }
     </style>
 </head>
-
 <body>
-    {{ $customer->customer_name }}
-
+    <h1>চালান রশিদ</h1>
+    <p>নাম: মোঃ হাসান</p>
+    <p>ফোন: ০১৭৬৩৪১৭০৭১</p>
+    <p>পরিমাণ: ৫০০ টাকা</p>
+    <p>ধন্যবাদ!</p>
 </body>
-
 </html>
