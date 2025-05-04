a4f24fb6fe502307a835100cb3a997a80277fd32
Sumon Mahmud <sclsumonislam@gmail.com>
2025-05-04 23:15:36 +0600

    add invoice

--- resources/views/reports/pdf.blade.php
+++ resources/views/reports/pdf.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Invoice</title>
@@ -12,74 +13,229 @@
             font-style: normal;
         }
 
-        body {
-            font-family: 'NotoSans', sans-serif;
-            font-size: 14px;
-            color: #333;
-            margin: 20px;
+        * {
+            font-family: 'Roboto', sans-serif;
+            line-height: 26px;
+            font-size: 15px
         }
 
-        .header {
-            text-align: center;
-            font-size: 24px;
-            font-weight: bold;
-            margin-bottom: 30px;
-            color: #2c3e50;
+        .custom--table {
+            width: 100%;
+            color: inherit;
+            vertical-align: top;
+            font-weight: 400;
+            border-collapse: collapse;
+            border-bottom: 2px solid #ddd;
+            margin-top: 0;
         }
 
-        .section-title {
+        .table-title {
             font-size: 18px;
             font-weight: 600;
-            color: #2c3e50;
-            margin-top: 20px;
-            margin-bottom: 10px;
-            border-bottom: 2px solid #3498db;
-            padding-bottom: 5px;
+            line-height: 32px;
+            margin-bottom: 10px
         }
 
-        table {
-            width: 100%;
-            border-collapse: collapse;
-            margin-bottom: 20px;
+        .custom--table thead {
+            font-weight: 700;
+            background: inherit;
+            color: inherit;
+            font-size: 16px;
+            font-weight: 500
         }
 
-        th {
-            background-color: #3498db;
-            color: white;
+        .custom--table tbody {
+            border-top: 0;
+            overflow: hidden;
+            border-radius: 10px;
+        }
+
+        .custom--table thead tr {
+            border-top: 2px solid #ddd;
+            border-bottom: 2px solid #ddd;
+            text-align: left
+        }
+
+        .custom--table thead tr th {
+            border-top: 2px solid #ddd;
+            border-bottom: 2px solid #ddd;
             text-align: left;
-            padding: 8px;
+            font-size: 16px;
+            padding: 10px 0
         }
 
-        td {
-            padding: 8px;
-            border: 1px solid #ccc;
+        .custom--table tbody tr {
+            vertical-align: top;
         }
 
-        .footer {
-            text-align: center;
-            font-size: 13px;
-            color: #888;
-            margin-top: 40px;
+        .custom--table tbody tr td {
+            font-size: 14px;
+            line-height: 18px vertical-align:top 10
         }
 
-        .signatures {
-            margin-top: 50px;
+        .custom--table tbody tr td:last-child {
+            padding-bottom: 10px;
+        }
+
+        .custom--table tbody tr td .data-span {
+            font-size: 14px;
+            font-weight: 500;
+            line-height: 18px;
+        }
+
+        .custom--table tbody .table_footer_row {
+            border-top: 2px solid #ddd;
+            margin-bottom: 10px !important;
+            padding-bottom: 10px !important
+        }
+
+        /* invoice area */
+        .invoice-area {
+            padding: 10px 0
+        }
+
+        .invoice-wrapper {
+            max-width: 650px;
+            margin: 0 auto;
+            box-shadow: 0 0 10px #f3f3f3;
+            padding: 0px;
+        }
+
+        .invoice-header {
+            margin-bottom: 40px;
+        }
+
+        .invoice-flex-contents {
             display: flex;
+            align-items: center;
             justify-content: space-between;
+            gap: 24px;
+            flex-wrap: wrap;
         }
 
-        .signature-box {
-            text-align: center;
-            width: 45%;
+        .invoice-title {
+            font-size: 25px;
+            font-weight: 700
         }
 
-        .signature-line {
-            margin-top: 40px;
-            border-top: 1px solid #000;
-            padding-top: 5px;
+        .invoice-details {
+            margin-top: 20px
+        }
+
+        .invoice-details-flex {
+            display: flex;
+            align-items: flex-start;
+            justify-content: space-between;
+            gap: 24px;
+            flex-wrap: wrap;
+        }
+
+        .invoice-details-title {
+            font-size: 18px;
+            font-weight: 700;
+            line-height: 32px;
+            color: #333;
+            margin: 0;
+            padding: 0
+        }
+
+        .invoice-single-details {
+            padding: 10px
+        }
+
+        .details-list {
+            margin: 0;
+            padding: 0;
+            list-style: none;
+            margin-top: 10px;
+        }
+
+        .details-list .list {
+            font-size: 14px;
+            font-weight: 400;
+            line-height: 18px;
+            color: #666;
+            margin: 0;
+            padding: 0;
+            transition: all .3s;
+        }
+
+        .details-list .list strong {
+            font-size: 14px;
+            font-weight: 500;
+            line-height: 18px;
+            color: #666;
+            margin: 0;
+            padding: 0;
+            transition: all .3s
+        }
+
+        .details-list .list a {
+            display: inline-block;
+            color: #666;
+            transition: all .3s;
+            text-decoration: none;
+            margin: 0;
+            line-height: 18px
+        }
+
+        .item-description {
+            margin-top: 10px;
+            padding: 10px;
+        }
+
+        .products-item {
+            text-align: left
+        }
+
+        .invoice-total-count .list-single {
+            display: flex;
+            align-items: center;
+            gap: 30px;
+            font-size: 16px;
+            line-height: 28px
+        }
+
+        .invoice-subtotal {
+            border-bottom: 2px solid #ddd;
+            padding-bottom: 15px
+        }
+
+        .invoice-total {
+            padding-top: 10px
+        }
+
+        .invoice-flex-footer {
+            display: flex;
+            align-items: flex-start;
+            justify-content: space-between;
+            flex-wrap: wrap;
+            gap: 30px;
+        }
+
+        .single-footer-item {
+            flex: 1
+        }
+
+        .single-footer {
+            display: flex;
+            align-items: center;
+            gap: 10px
+        }
+
+        .single-footer .icon {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 30px;
+            width: 30px;
+            font-size: 16px;
+            background-color: #000e8f;
+            color: #fff
         }
     </style>
 </head>
+
 <body>
 
     <div class="header">
@@ -146,18 +302,18 @@
         </thead>
         <tbody>
             @foreach ($installments as $index => $inst)
-            <tr>
-                <td>{{ $index + 1 }}</td>
-                <td>{{ \Carbon\Carbon::parse($inst->due_date)->format('F Y') }}</td>
-                <td>{{ number_format($inst->amount) }} Tk</td>
-                <td>
-                    @if ($inst->status == 'paid')
-                        Paid
-                    @else
-                        Due
-                    @endif
-                </td>
-            </tr>
+                <tr>
+                    <td>{{ $index + 1 }}</td>
+                    <td>{{ \Carbon\Carbon::parse($inst->due_date)->format('F Y') }}</td>
+                    <td>{{ number_format($inst->amount) }} Tk</td>
+                    <td>
+                        @if ($inst->status == 'paid')
+                            Paid
+                        @else
+                            Due
+                        @endif
+                    </td>
+                </tr>
             @endforeach
         </tbody>
     </table>
@@ -176,4 +332,5 @@
     </div>
 
 </body>
+
 </html>
