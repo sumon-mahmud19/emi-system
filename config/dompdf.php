11eb5dc51144c2b6fa08655764ca9cb7c617013f
Sumon Mahmud <sclsumonislam@gmail.com>
2025-05-04 22:55:58 +0600

    add invoice

--- config/dompdf.php
+++ config/dompdf.php
@@ -2,54 +2,21 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | PDF Driver
-    |--------------------------------------------------------------------------
-    |
-    | Supported: "dompdf"
-    |
-    */
+    'font_dir' => public_path('fonts'),
+    'font_cache' => storage_path('fonts'),
+    'default_font' => 'NotoSansBengali',
 
-    'driver' => 'dompdf',
 
-    /*
-    |--------------------------------------------------------------------------
-    | DomPDF Settings
-    |--------------------------------------------------------------------------
-    |
-    | These settings are passed directly to DomPDF.
-    |
-    */
-
-    'dompdf' => [
-        'show_warnings' => false,
-        'orientation' => 'portrait',
-        'defines' => [
-            // Load Bangla font support
-            'DOMPDF_ENABLE_REMOTE' => true,
-            'DOMPDF_UNICODE_ENABLED' => true,
-            'DOMPDF_FONT_DIR' => public_path('fonts/'),
-            'DOMPDF_FONT_CACHE' => storage_path('fonts/'),
-            'DOMPDF_DEFAULT_MEDIA_TYPE' => 'screen',
-            'DOMPDF_DEFAULT_PAPER_SIZE' => 'a4',
-            'DOMPDF_DEFAULT_FONT' => 'notosansbengali',
-            'DOMPDF_DPI' => 96,
-            'DOMPDF_ENABLE_HTML5PARSER' => true,
-            'DOMPDF_ENABLE_FONTSUBSETTING' => true,
+    'font_data' => [
+        'notosansbengali' => [
+            'R' => 'NotoSansBengali-Regular.ttf',
+            'useOTL' => 0xFF, // Required for complex scripts like Bangla
+            'useKashida' => 75,
         ],
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | PDF Generation Options
-    |--------------------------------------------------------------------------
-    |
-    | These options are used when generating the PDF.
-    |
-    */
 
-    'default_font' => 'notosansbengali',
 
-    'paper' => 'a4',
+
+
 ];
