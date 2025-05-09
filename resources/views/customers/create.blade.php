e7c0bee21a288efdf10e71f33dfae9b3ed22ead8
Sumon Mahmud <sclsumonislam@gmail.com>
2025-05-09 18:55:48 +0600

    home design change

--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -7,85 +7,79 @@
         <form action="{{ route('customers.store') }}" method="POST" enctype="multipart/form-data">
             @csrf
 
-            <div class="mb-3">
-                <label for="customer_name">Customer Name</label>
-                <input type="text" name="customer_name" class="form-control">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="mb-3">
+                        <label for="customer_name">Customer Name</label>
+                        <input type="text" name="customer_name" class="form-control" value="{{ old('customer_name') }}">
+                        @error('customer_name')
+                            <div class="alert alert-danger mt-2">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="customer_id">Register ID</label>
+                        <input type="number" name="customer_id" class="form-control" value="{{ old('customer_id') }}">
+                        @error('customer_id')
+                            <div class="alert alert-danger mt-2">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="customer_phone">Customer Phone</label>
+                        <input type="text" name="customer_phone" class="form-control" value="{{ old('customer_phone') }}">
+                        @error('customer_phone')
+                            <div class="alert alert-danger mt-2">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="landlord_name">Landlord Name</label>
+                        <input type="text" name="landlord_name" class="form-control" value="{{ old('landlord_name') }}">
+                        @error('landlord_name')
+                            <div class="alert alert-danger mt-2">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="mb-3">
+                        <label for="customer_image">Customer Image</label>
+                        <input type="file" name="customer_image" class="form-control">
+                        @error('customer_image')
+                            <div class="alert alert-danger mt-2">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="location_id">Location</label>
+                        <select name="location_id" class="form-control">
+                            <option value="">Select Location</option>
+                            @foreach ($locations as $location)
+                                <option value="{{ $location->id }}" {{ old('location_id') == $location->id ? 'selected' : '' }}>
+                                    {{ $location->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                        @error('location_id')
+                            <div class="alert alert-danger mt-2">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="location_details">Address Details</label>
+                        <input type="text" name="location_details" class="form-control" value="{{ old('location_details') }}">
+                        @error('location_details')
+                            <div class="alert alert-danger mt-2">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
             </div>
 
-            @error('customer_name')
-                <div class="alert alert-danger">
-                    {{ $message }}
-                </div>
-            @enderror
-
-            <div class="mb-3">
-                <label for="customer_id">Register ID</label>
-                <input type="number" name="customer_id" class="form-control">
+            <div class="d-flex justify-content-between mt-3">
+                <button type="submit" class="btn btn-primary">Save</button>
+                <a href="{{ route('customers.index') }}" class="btn btn-secondary">Cancel</a>
             </div>
-            @error('customer_id')
-                <div class="alert alert-danger">
-                    {{ $message }}
-                </div>
-            @enderror
-
-            <div class="mb-3">
-                <label for="customer_phone">Customer Phone</label>
-                <input type="text" name="customer_phone" class="form-control">
-            </div>
-            @error('customer_phone')
-                <div class="alert alert-danger">
-                    {{ $message }}
-                </div>
-            @enderror
-
-            <div class="mb-3">
-                <label for="customer_image">Customer Image</label>
-                <input type="file" name="customer_image" class="form-control">
-            </div>
-            @error('customer_image')
-                <div class="alert alert-danger">
-                    {{ $message }}
-                </div>
-            @enderror
-
-            <div class="mb-3">
-                <label for="landlord_name">Landlord Name</label>
-                <input type="text" name="landlord_name" class="form-control">
-            </div>
-            @error('landlord_name')
-                <div class="alert alert-danger">
-                    {{ $message }}
-                </div>
-            @enderror
-
-            <div class="mb-3">
-                <label for="location_id">Location</label>
-                <select name="location_id" class="form-control">
-                    <option value="">Select Location</option>
-                    @foreach ($locations as $location)
-                        <option value="{{ $location->id }}">{{ $location->name }}</option>
-                    @endforeach
-                </select>
-            </div>
-            @error('location_id')
-                <div class="alert alert-danger">
-                    {{ $message }}
-                </div>
-            @enderror
-
-
-            <div class="mb-3">
-                <label for="location_details">Address Details</label>
-                <input type="text" name="location_details" class="form-control">
-            </div>
-            @error('location_details')
-                <div class="alert alert-danger">
-                    {{ $message }}
-                </div>
-            @enderror
-
-            <button type="submit" class="btn btn-primary">Save</button>
-            <a href="{{ route('customers.index') }}" class="btn btn-secondary">Cancel</a>
         </form>
     </div>
 @endsection
