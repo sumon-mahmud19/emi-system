75c8550d987d6555320c8c53da008349786efd78
Sumon Mahmud <sclsumonislam@gmail.com>
2025-05-09 21:39:20 +0600

    design customer create page

--- resources/views/customers/partials/customer_table_rows.blade.php
+++ resources/views/customers/partials/customer_table_rows.blade.php
@@ -4,22 +4,6 @@
         <td>{{ $customer->customer_name }}</td>
         <td>{{ $customer->customer_id }}</td>
         <td>
-            <a href="tel:{{ $customer->customer_phone }}">{{ $customer->customer_phone }}</a>
-        </td>
-        <td>
-            <a class="show-customer-modal" data-bs-toggle="modal" data-bs-target="#customerModal"
-               data-name="{{ $customer->customer_name }}"
-               data-id="{{ $customer->customer_id }}"
-               data-phone="tel:{{ $customer->customer_phone }}"
-               data-location="{{ $customer->location->name ?? 'N/A' }}"
-               data-image="{{ asset($customer->customer_image ?? 'images/default.png') }}">
-                <img src="{{ asset($customer->customer_image ?? 'images/default.png') }}"
-                     class="img-fluid rounded-circle"
-                     style="height: 50px; width: 50px; object-fit: cover;">
-            </a>
-        </td>
-        <td>{{ $customer->location->name ?? 'N/A' }}</td>
-        <td>
             @can('customer-edit')
                 <a href="{{ route('customers.edit', $customer->id) }}" class="btn btn-warning btn-sm">
                     <i class="fas fa-edit"></i>
@@ -38,7 +22,27 @@
             @endcan
 
             <a href="{{ route('customers.emi_plans', $customer->id) }}" class="btn btn-sm btn-primary">
-                <i class="fas fa-list"></i> EMI Details
+                <i class="fas fa-eye"></i>
+            </a>
+        </td>
+        <td>
+            <a href="tel:{{ $customer->customer_phone }}">{{ $customer->customer_phone }}</a>
+        </td>
+        <td>
+            <a class="show-customer-modal" data-bs-toggle="modal" data-bs-target="#customerModal"
+               data-name="{{ $customer->customer_name }}"
+               data-id="{{ $customer->customer_id }}"
+               data-phone="tel:{{ $customer->customer_phone }}"
+               data-location="{{ $customer->location->name ?? 'N/A' }}"
+               data-image="{{ asset($customer->customer_image ?? 'images/default.png') }}">
+                <img src="{{ asset($customer->customer_image ?? 'images/default.png') }}"
+                     class="img-fluid rounded-circle"
+                     style="height: 50px; width: 50px; object-fit: cover;">
+            </a>
+        </td>
+        <td>
+            <a href="{{ route('customers.show', $customer->location->id ?? '#') }}">
+                {{ $customer->location->name ?? 'N/A' }}
             </a>
         </td>
     </tr>
