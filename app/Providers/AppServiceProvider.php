4b51599259862af631dd5837ba117f5fa10b7c14
Sumon Mahmud <sclsumonislam@gmail.com>
2025-05-09 18:21:48 +0600

    design invoice

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        Paginator::useBootstrapFive();
+        Paginator::useTailwind();
     }
 
     /**
