b9476f7200297e8c402d461b2c4fff154cbf41e2
Sumon Mahmud <sclsumonislam@gmail.com>
2025-05-14 21:53:47 +0600

    add defult pic on customer td

--- resources/views/customers/partials/customer_table_rows.blade.php
+++ resources/views/customers/partials/customer_table_rows.blade.php
@@ -14,8 +14,7 @@
                 <form action="{{ route('customers.destroy', $customer->id) }}" method="POST" style="display: inline;">
                     @csrf
                     @method('DELETE')
-                    <button type="submit" class="btn btn-danger btn-sm"
-                            onclick="return confirm('Are you sure?')">
+                    <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure?')">
                         <i class="fas fa-trash-alt"></i>
                     </button>
                 </form>
@@ -30,14 +29,13 @@
         </td>
         <td>
             <a class="show-customer-modal" data-bs-toggle="modal" data-bs-target="#customerModal"
-               data-name="{{ $customer->customer_name }}"
-               data-id="{{ $customer->customer_id }}"
-               data-phone="tel:{{ $customer->customer_phone }}"
-               data-location="{{ $customer->location->name ?? 'N/A' }}"
-               data-image="{{ asset($customer->customer_image ?? 'images/default.png') }}">
-                <img src="{{ asset($customer->customer_image ?? 'images/default.png') }}"
-                     class="img-fluid rounded-circle"
-                     style="height: 50px; width: 50px; object-fit: cover;">
+                data-name="{{ $customer->customer_name }}" data-id="{{ $customer->customer_id }}"
+                data-phone="tel:{{ $customer->customer_phone }}"
+                data-location="{{ $customer->location->name ?? 'N/A' }}"
+                data-image="{{ asset($customer->customer_image ?? 'images/default.png') }}">
+                <img src="{{ asset($customer->customer_image ?? 'image/profile.png') }}"
+                    class="img-fluid rounded-circle" style="height: 50px; width: 50px; object-fit: cover;">
+
             </a>
         </td>
         <td>
