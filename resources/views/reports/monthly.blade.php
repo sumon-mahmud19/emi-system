9193cae5192dc1abee4046c5992c87da932ad8d6
Sumon Mahmud <sclsumonislam@gmail.com>
2025-05-19 22:34:14 +0600

    update installment controller

--- resources/views/reports/monthly.blade.php
+++ resources/views/reports/monthly.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
     <div class="container mt-3">
@@ -133,4 +133,4 @@
             }
         });
     </script>
-@endsection
+@endsection --}}
