c49fe002d89a916c9b067b68d971603637d97099
Sumon Mahmud <sclsumonislam@gmail.com>
2025-05-23 09:14:04 +0600

    update installment page

--- app/Models/InstallmentPayment.php
+++ app/Models/InstallmentPayment.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class InstallmentPayment extends Model
 {
-    //
+    protected $fillable = ['installment_id', 'amount', 'paid_at'];
+
+    public function installment()
+    {
+        return $this->belongsTo(Installment::class);
+    }
 }
