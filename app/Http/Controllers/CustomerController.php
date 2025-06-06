0deee2df2771e9c7f286d6966699898c8da1ec5c
sclsumonislam@gmail.com <sclsumonislam@gmail.com>
2025-06-06 09:05:54 +0600

    added beautiful toster alert

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -113,10 +113,11 @@ class CustomerController extends Controller
             'location_details' => $validated['location_details'],
         ]);
 
+        toastr()->addSuccess('Customer created!');
         // Mail::to('sclsumonislam@gmail.com')->send(new NewCustomerNotification($customer));
 
         // Redirect to the customer index page with a success message
-        return redirect()->route('customers.index')->with('success', 'Customer created successfully.');
+        return redirect()->route('customers.index');
     }
 
     /**
